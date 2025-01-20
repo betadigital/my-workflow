695b1dd56cca3d824c59a72280bce2858318d401
kriti-srivas <kriti.srivastava@betadigital.com.au>
2025-01-21 10:14:25 +1100

    BTP destination connectivity changes
    Updated routes and controller to connect to BTP Destinations

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -15,75 +15,77 @@ sap.ui.define([
         _onRouteMatched: function (oEvent) {
             const instanceId = oEvent.getParameter("arguments").rowId;
             const oWorkflowModel = this.getOwnerComponent().getModel("workflowModel");
-            this.getAPIData(oWorkflowModel, instanceId);             
+            this.getAPIData(oWorkflowModel, instanceId);
         },
 
-        getAPIData: async function(oWorkflowModel, instanceId){
-            let workflowData = await this._fetchWorkflowData(instanceId);
+        getAPIData: async function (oWorkflowModel, instanceId) {
+            const prefixUrl = this._getExternalServiceRuntimeBaseURL();
+            let workflowData = await this._fetchWorkflowData(prefixUrl, instanceId);
             oWorkflowModel.setProperty("/context", workflowData)
             let uploadedFolder = workflowData ? workflowData.startEvent.uploadFiles : null;
-            if(uploadedFolder){
+            if (uploadedFolder) {
                 var oBusyModel = this.getView().getModel("busyModel");
                 oBusyModel.setProperty("/isBusy", true);
                 let folderId = uploadedFolder.split(":").pop();
-                let repoId = await this._fetchDMSRepo();
+                let repoId = await this._fetchDMSRepo(prefixUrl);
                 oWorkflowModel.setProperty("/filesRepo", repoId);
-                let uploadedFiles = await this._fetchFileDataFromDMS(repoId, folderId);
+                let uploadedFiles = await this._fetchFileDataFromDMS(prefixUrl, repoId, folderId);
                 const aFiles = this.formatFileCollection(uploadedFiles);
                 oWorkflowModel.setProperty("/filesCollection", aFiles);
                 oBusyModel.setProperty("/isBusy", false);
             }
         },
-        
-        _fetchWorkflowData: async function(instanceId) {
+
+        _fetchWorkflowData: async function (prefixUrl, instanceId) {
             const requestOptions = {
                 method: "GET",
                 redirect: "follow"
             };
+
             try {
-                const response = await fetch(`/workflow_api/v1/workflow-instances/${instanceId}/context`, requestOptions)
-                if(!response.ok){
+                const response = await fetch(`${prefixUrl}/SAP_Build_Workflow/v1/workflow-instances/${instanceId}/context`, requestOptions)
+                if (!response.ok) {
                     throw new Error(`Error in fetching workflow instance with status: ${response.status}`)
                 }
                 const data = await response.json()
                 return data;
-            } catch(error){
+            } catch (error) {
                 console.error(error.message)
-            }  
+            }
         },
 
-        _fetchDMSRepo: async function(){
+        _fetchDMSRepo: async function (prefixUrl) {
             const requestOptions = {
                 method: "GET",
                 redirect: "follow"
             };
             try {
-                const response = await fetch('/dms_api/browser', requestOptions)
-                if(!response.ok){
+                const response = await fetch(`${prefixUrl}/SAP_DMS/browser`, requestOptions)
+                if (!response.ok) {
                     throw new Error(`Error in fetching dms repository with status: ${response.status}`)
                 }
                 const data = await response.json();
-                return Object.keys(data)[0]; 
-            } catch(error){
+                return Object.keys(data)[0];
+            } catch (error) {
                 console.error(error.message)
-            } 
+            }
         },
 
-        _fetchFileDataFromDMS: async function(repoId, folderId){
+        _fetchFileDataFromDMS: async function (prefixUrl, repoId, folderId) {
             const requestOptions = {
                 method: "GET",
                 redirect: "follow"
             };
             try {
-                const response = await fetch(`/dms_api/browser/${repoId}/root?cmisselector=children&objectId=${folderId}`, requestOptions)
-                if(!response.ok){
+                const response = await fetch(`${prefixUrl}/SAP_DMS/browser/${repoId}/root?cmisselector=children&objectId=${folderId}`, requestOptions)
+                if (!response.ok) {
                     throw new Error(`Error in fetching files from dms repository with status: ${response.status}`)
                 }
                 const data = await response.json();
-                return data; 
-            } catch(error){
+                return data;
+            } catch (error) {
                 console.error(error.message)
-            }  
+            }
         },
 
         onNavBack: function () {
@@ -98,16 +100,16 @@ sap.ui.define([
             }
         },
 
-        concatSponsorName: function(sFirstName, sLastName){
+        concatSponsorName: function (sFirstName, sLastName) {
             return `${sFirstName} ${sLastName}`
         },
 
-        formatFileCollection: function(uploadedFiles) {
+        formatFileCollection: function (uploadedFiles) {
             if (!uploadedFiles) {
                 return [];
             }
             let modifiedFiles = [];
-            for(let file of uploadedFiles.objects){
+            for (let file of uploadedFiles.objects) {
                 let modifiedFileObj = {};
                 modifiedFileObj.fileName = file.object.properties['cmis:name'].value
                 modifiedFileObj.fileObjectId = file.object.properties['cmis:objectId'].value
@@ -116,31 +118,45 @@ sap.ui.define([
             return modifiedFiles;
         },
 
-        onAttachmentPress: function(oEvent){
+        onAttachmentPress: function (oEvent) {
             let oSource = oEvent.getSource();
             let oWorkflowModel = oSource.getModel('workflowModel');
             let fileName = oSource.getProperty('fileName');
             let fileObjectId = oSource.getProperty('url');
             let repoId = oWorkflowModel.getProperty('/filesRepo')
-            this._downloadFile(repoId, fileName, fileObjectId)
+            const prefixUrl = this._getExternalServiceRuntimeBaseURL();
+            this._downloadFile(prefixUrl, repoId, fileName, fileObjectId)
         },
 
-        _downloadFile: async function(repoId, fileName, fileObjectId){
+        _downloadFile: async function (prefixUrl, repoId, fileName, fileObjectId) {
             const requestOptions = {
                 method: "GET",
                 redirect: "follow"
             };
-            
-            fetch(`/dms_api/browser/${repoId}/root?cmisselector=content&objectId=${fileObjectId}&download=attachment`, requestOptions)
-            .then((response) => response.blob())
-            .then((oBlob) => {
-                const oLink = document.createElement("a");
-                oLink.href = URL.createObjectURL(oBlob);
-                oLink.download = fileName;
-                oLink.click();
-                URL.revokeObjectURL(oLink.href);
-            })
-            .catch((error) => console.error("Error in downloading file with error message:", error));
+
+            fetch(`${prefixUrl}/SAP_DMS/browser/${repoId}/root?cmisselector=content&objectId=${fileObjectId}&download=attachment`, requestOptions)
+                .then((response) => response.blob())
+                .then((oBlob) => {
+                    const oLink = document.createElement("a");
+                    oLink.href = URL.createObjectURL(oBlob);
+                    oLink.download = fileName;
+                    oLink.click();
+                    URL.revokeObjectURL(oLink.href);
+                })
+                .catch((error) => console.error("Error in downloading file with error message:", error));
+        },
+
+        _getExternalServiceRuntimeBaseURL: function () {
+            var oComponent = sap.ui.core.Component.getOwnerComponentFor(this.getView());
+            if (oComponent) {
+                var sAppId = oComponent.getManifestEntry("/sap.app/id");
+                var sAppPath = sAppId.replaceAll(".", "/");
+                var sAppModulePath = jQuery.sap.getModulePath(sAppPath);
+                return sAppModulePath;
+            } else {
+                console.error("Component could not be found.");
+                return "";
+            }
         }
     });
 });
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,103 +5,82 @@ sap.ui.define([
   "use strict";
 
   return Controller.extend("sap.ui.workflowtrial.controller.View1", {
-    onInit: function () {    
-       const oWorkflowModel = this.getOwnerComponent().getModel("workflowModel");   
-       this.getDataFromAPI()
-       .then((data)=>{
+    onInit: function () {
+      const oWorkflowModel = this.getOwnerComponent().getModel("workflowModel");
+      this.getDataFromAPI()
+        .then((data) => {
           const oData = {
             rows: data
           }
           oWorkflowModel.setProperty("/workflowData", oData)
           this.getView().setModel(oWorkflowModel);
-       })
-       .catch((error)=>{
-        console.error(error.message)
-       })
-  },
+        })
+        .catch((error) => {
+          console.error(error.message)
+        })
+    },
 
-  onRowSelect: function (oEvent) {
-    const oSelectedItem = oEvent.getParameter("listItem");
-    const oContext = oSelectedItem.getBindingContext('workflowModel');
-    const instanceId = oContext.getProperty("id");
-    UIComponent.getRouterFor(this).navTo("details", { rowId: instanceId });
-  },
+    onRowSelect: function (oEvent) {
+      const oSelectedItem = oEvent.getParameter("listItem");
+      const oContext = oSelectedItem.getBindingContext('workflowModel');
+      const instanceId = oContext.getProperty("id");
+      UIComponent.getRouterFor(this).navTo("details", { rowId: instanceId });
+    },
 
-  getDataFromAPI: async function (){
-      const data = await this._fetchApiData()
-      // const uaaToken = await this._fetchUAABearerToken()
-      // const user = await this._fetchUserData(uaaToken)
-      // console.log(user)
+    getDataFromAPI: async function () {
+      const prefixUrl = this._getExternalServiceRuntimeBaseURL();
+      const user = await this._fetchUserData(prefixUrl);
+      const data = await this._fetchApiData(prefixUrl, user)
       return data;
-  },
+    },
 
-  _fetchUAABearerToken: async function(){
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.append("Cookie", "__VCAP_ID__=76109d11-6c8e-4a92-6487-e31d; JSESSIONID=F3E49EDF9463822CA9D07E20B161B0A1; X-Uaa-Csrf=Q2f062qeQwRwAz2DRiRb5v; __VCAP_ID__=42dff77c-a804-4072-7d3f-3e9f");
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("grant_type", "client_credentials");
-    urlencoded.append("client_id", "sb-sapuiworkflowtrial!t54335");
-    urlencoded.append("client_secret", "edc9a7fe-bbe8-4ff0-a81a-42e99617c874$yyLufIO0MZbUQcaAtjqW42PPSDfFo7nnBGYv17rgDUI=");
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow"
-    };
-
-    try {
-      const response = await fetch("/token_api", requestOptions)
-      if(!response.ok){
-          throw new Error(`Error in fetching token with status: ${response.status}`)
+    _getExternalServiceRuntimeBaseURL: function () {
+      var oComponent = sap.ui.core.Component.getOwnerComponentFor(this.getView());
+      if (oComponent) {
+        var sAppId = oComponent.getManifestEntry("/sap.app/id");
+        var sAppPath = sAppId.replaceAll(".", "/");
+        var sAppModulePath = jQuery.sap.getModulePath(sAppPath);
+        return sAppModulePath;
+      } else {
+        console.error("Component could not be found.");
+        return "";
       }
-      const data = await response.json()
-      return data.access_token;
-    } catch(error){
-        console.error(error.message)
-    } 
-  },
+    },
 
-  _fetchApiData: async function () {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow"
-    };
-    const user = 'shaun.bradridge@betadigital.com.au'
-    try {
-      const response = await fetch(`/workflow_api/v1/workflow-instances?startedBy=${user}`, requestOptions)
-      if(!response.ok){
+    _fetchApiData: async function (prefixUrl, user) {
+      const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      const url = user ? `${prefixUrl}/SAP_Build_Workflow/v1/workflow-instances?startedBy=${user.email}`
+        : `${prefixUrl}/SAP_Build_Workflow/v1/workflow-instances`;
+      try {
+        const response = await fetch(url, requestOptions)
+        if (!response.ok) {
           throw new Error(`Error in fetching workflow instances with status: ${response.status}`)
-      }
-      const data = await response.json()
-      return data;
-    } catch(error){
+        }
+        const data = await response.json()
+        return data;
+      } catch (error) {
         console.error(error.message)
-    } 
-  },
-
-  _fetchUserData: async function(token){
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + token);
-    myHeaders.append("Accept", "application/json")
-    
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders
-    };
-
-    try {
-      const response = await fetch("/user_api/userinfo", requestOptions)
-      if(!response.ok){
-          throw new Error(`Error in user data with status: ${response.status}`)
       }
-      const user = await response.json();
-      return user;
-    } catch(error){
+    },
+
+    _fetchUserData: async function (prefixUrl) {
+      const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      try {
+        const response = await fetch(`${prefixUrl}/user_api/currentUser`, requestOptions)
+        if (!response.ok) {
+          throw new Error(`Error in fetching workflow instances with status: ${response.status}`)
+        }
+        const user = await response.json();
+        return user;
+      } catch (error) {
         console.error(error.message)
-    } 
-  }
-  
+      }
+    }
   });
 });
